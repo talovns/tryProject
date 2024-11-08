bd2fbf3f4cb7e13485d620301054fb4d0924996c
talovns <emineklok@gmail.com>
2024-11-08 18:30:47 +0300

    1

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func visokosnygod(x int) string {
+	if (x%400 == 0) && (x%100 == 0) {
+		return "true"
+	} else if (x%4 == 0) && (x%100 != 0) {
+		return "true"
+	} else {
+		return "false"
+	}
+}
+
+var a int
+
 func main() {
-	fmt.Print('1')
+	fmt.Scan(&a)
+	fmt.Print(visokosnygod(a))
 }
