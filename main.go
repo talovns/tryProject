6acd68e237ffb4713df5db0b4c975ebb97b8eca8
talovns <emineklok@gmail.com>
2024-11-22 01:21:30 +0300

    compleate all

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"math"
-// 	"strconv"
-// )
+import "fmt"
 
 // func PerevodStrVvInt(x string) float64 {
 // 	st := "0123456789"
@@ -21,45 +17,45 @@
 // 	return pr
 // }
 
-// // func ReturnZnacheniaVirajenia(lst string) (rez string) {
-// // 	if strings.Count(lst, "*") == 0 && strings.Count(lst, "/") == 0 {
-// // 		rez = lst
-// // 	} else {
-// // 		for i := 0; i < len(lst)-1; i++ {
-// // 			if string(lst[i]) == "/" {
-// // 				umnoj := PerevodStrVvInt(string(lst[i-1])) / PerevodStrVvInt(string(lst[i+1]))
-// // 				pr := strconv.Itoa(umnoj)
-// // 				rez = lst[:i-1] + pr + lst[i+2:]
-// // 			}
-// // 			if string(lst[i]) == "/" {
-// // 				umnoj := PerevodStrVvInt(string(lst[i-1])) / PerevodStrVvInt(string(lst[i+1]))
-// // 				pr := strconv.Itoa(umnoj)
-// // 				rez = lst[:i-1] + pr + lst[i+2:]
-// // 			}
-// // 		}
-// // 	}
-// // 	return ReturnZnacheniaVirajenia(rez)
-// // }
-
-// func ReturnZnacheniaVirajenia(lst []string) []string {
-// 	for len(lst) != 1 {
-
+// func ReturnZnacheniaVirajenia(lst string) (rez string) {
+// 	if strings.Count(lst, "*") == 0 && strings.Count(lst, "/") == 0 {
+// 		rez = lst
+// 	} else {
 // 		for i := 0; i < len(lst)-1; i++ {
-// 			if string(lst[i]) == "*" {
-// 				umnoj := PerevodStrVvInt(string(lst[i-1])) * PerevodStrVvInt(string(lst[i+1]))
-// 				pr := strconv.FormatFloat(umnoj, 'f', -1, 64)
+// 			if string(lst[i]) == "/" {
+// 				umnoj := PerevodStrVvInt(string(lst[i-1])) / PerevodStrVvInt(string(lst[i+1]))
+// 				pr := strconv.Itoa(umnoj)
 // 				rez = lst[:i-1] + pr + lst[i+2:]
 // 			}
 // 			if string(lst[i]) == "/" {
-// 				umnoj := (PerevodStrVvInt(string(lst[i-1]))) / (PerevodStrVvInt(string(lst[i+1])))
-// 				pr := strconv.FormatFloat(umnoj, 'f', -1, 64)
+// 				umnoj := PerevodStrVvInt(string(lst[i-1])) / PerevodStrVvInt(string(lst[i+1]))
+// 				pr := strconv.Itoa(umnoj)
 // 				rez = lst[:i-1] + pr + lst[i+2:]
 // 			}
 // 		}
-// 		lst = rez
 // 	}
-// 	return lst
+// 	return ReturnZnacheniaVirajenia(rez)
 // }
+
+// // func ReturnZnacheniaVirajenia(lst []string) []string {
+// // 	for len(lst) != 1 {
+
+// // 		for i := 0; i < len(lst)-1; i++ {
+// // 			if string(lst[i]) == "*" {
+// // 				umnoj := PerevodStrVvInt(string(lst[i-1])) * PerevodStrVvInt(string(lst[i+1]))
+// // 				pr := strconv.FormatFloat(umnoj, 'f', -1, 64)
+// // 				rez = lst[:i-1] + pr + lst[i+2:]
+// // 			}
+// // 			if string(lst[i]) == "/" {
+// // 				umnoj := (PerevodStrVvInt(string(lst[i-1]))) / (PerevodStrVvInt(string(lst[i+1])))
+// // 				pr := strconv.FormatFloat(umnoj, 'f', -1, 64)
+// // 				rez = lst[:i-1] + pr + lst[i+2:]
+// // 			}
+// // 		}
+// // 		lst = rez
+// // 	}
+// // 	return lst
+// // }
 
 // // func esheReturn(lst string) (rez string) {
 // // 	for (strings.Count(lst, "-") > 0) && (strings.Count(lst, "+") > 0) {
@@ -83,7 +79,67 @@
 
 // // переделать все обратно в список для деления, так как будет флоат а там сложно будет ориентироваться по элементам
 // func main() {
-// 	lst := []string{"1", "+", "3"}
+// 	// lst := []string{"1", "+", "3"}
 // 	// fmt.Print(esheReturn(ReturnZnacheniaVirajenia(lst)))
-// 	fmt.Print(ReturnZnacheniaVirajenia(lst))
+// 	fmt.Print(ReturnZnacheniaVirajenia("1 + 3"))
 // }
+
+// занового ретерн:
+
+func VoBrtnPolski(st string) {
+	flag := false
+	for _, val := range st {
+		if val == '*' || val == '/' {
+			flag = true
+		}
+	}
+	var rez string = ""
+	if flag {
+		for i := 0; i < len(st); i++ {
+			if len(rez) == 0 {
+				if st[i] == '*' {
+					rez += string(st[i-1]) + " " + string(st[i+1]) + " *"
+				}
+				if st[i] == '/' {
+					rez += string(st[i-1]) + " " + string(st[i+1]) + " /"
+				}
+			} else {
+				if st[i] == '*' {
+					rez += " " + string(st[i+1]) + " *"
+				}
+				if st[i] == '/' {
+					rez += " " + string(st[i+1]) + " /"
+				}
+			}
+		}
+	}
+	for i := 0; i < len(st); i++ {
+		if len(rez) == 0 {
+			if st[i] == '+' {
+				rez += string(st[i-1]) + " " + string(st[i+1]) + " +"
+			}
+			if st[i] == '-' {
+				rez += string(st[i-1]) + " " + string(st[i+1]) + " -"
+			}
+		} else {
+			if st[i] == '+' {
+				rez += " " + string(st[i+1]) + " +"
+			}
+			if st[i] == '-' {
+				rez += " " + string(st[i+1]) + " -"
+			}
+		}
+	}
+	fmt.Print(rez)
+}
+
+// func retoorn([]string){
+
+// }
+
+// var lst = []string{}
+
+func main() {
+	VoBrtnPolski("3+2*5/7-1")
+	// retoorn(lst)
+}
--- two.go
+++ two.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"strconv"
 )
 
-var st string = " (3 + 2) * (4 - 2) / 2"
+var st string = " (3 + 2 / 8) * ( (4 - 2) / 2)"
 
 var pr string = "0123456789"
 
@@ -31,7 +33,7 @@ func strokaVSpisok(x string) []string {
 }
 
 func PerevodStrVvInt(x string) (rez int) {
-	st = "0123456789"
+	st := "0123456789"
 	for i := len(x) - 1; i >= 0; i-- {
 		for j := 0; j < len(st); j++ {
 			if st[j] == x[i] {
@@ -43,41 +45,167 @@ func PerevodStrVvInt(x string) (rez int) {
 	return
 }
 
-func ReturnZnacheniaVirajenia(lst []string) []string {
-	if len(lst) == 1 {
-		rez := lst
-		return rez
-	} else {
-		for i := 0; i < len(lst)-1; i++ {
-			if lst[i] == "*" {
-				//  сделано: надо перевести строки в числа перемножить и обратно в строку чтобы заретернить в список с новыми значениями
-				umnoj := PerevodStrVvInt(lst[i-1]) * PerevodStrVvInt(lst[i+1])
-				lst[i] = string(umnoj)
-				// понять как собрать список из срезлов +- так два раза сделать надо:
-				// users = append(users[:n], users[n+1:]...)
-				return lst[:i-1]
+func virajenia(x float64, y float64, s string) (rez string) {
+	switch s {
+	case "+":
+		rez = fmt.Sprint(x + y)
+	case "-":
+		rez = fmt.Sprint(x - y)
+	case "/":
+		rez = fmt.Sprint(x / y)
+	case "*":
+		rez = fmt.Sprint(x * y)
+	}
+	return
+}
+
+func VoBrtnPolski(strx string) string {
+	flag := false
+	for _, val := range strx {
+		if val == '*' || val == '/' {
+			flag = true
+		}
+	}
+	var st = []string{}
+	var pluss = ""
+	for p := 0; p < len(strx); p++ {
+		if strx[p] != '*' && strx[p] != '-' && strx[p] != '/' && strx[p] != '+' {
+			pluss += string(strx[p])
+			if p == len(strx)-1 {
+				st = append(st, pluss)
+				break
+			}
+		} else {
+			st = append(st, pluss)
+			st = append(st, string(strx[p]))
+			pluss = ""
+		}
+	}
+	var rez string = ""
+	if flag {
+		for i := 0; i < len(st); i++ {
+			if len(rez) == 0 {
+				if st[i] == "*" {
+					rez += string(st[i-1]) + " " + string(st[i+1]) + " *"
+				}
+				if st[i] == "/" {
+					rez += string(st[i-1]) + " " + string(st[i+1]) + " /"
+				}
+			} else {
+				if st[i] == "*" {
+					rez += " " + string(st[i+1]) + " *"
+				}
+				if st[i] == "/" {
+					rez += " " + string(st[i+1]) + " /"
+				}
 			}
 		}
 	}
-	return []string{}
+	for i := 0; i < len(st); i++ {
+		if len(rez) == 0 {
+			if st[i] == "+" {
+				rez += string(st[i-1]) + " " + string(st[i+1]) + " +"
+			}
+			if st[i] == "-" {
+				rez += string(st[i-1]) + " " + string(st[i+1]) + " -"
+			}
+		} else {
+			if st[i] == "+" {
+				rez += " " + string(st[i+1]) + " +"
+			}
+			if st[i] == "-" {
+				rez += " " + string(st[i+1]) + " -"
+			}
+		}
+	}
+	return rez
+}
+
+func polski(st string) string {
+	var rezk = ""
+	var lst1 = []string{}
+	var lst2 = []string{}
+	var prom = ""
+	for i := 0; i < len(st); i++ {
+		if st[i] != ' ' && st[i] != '*' && st[i] != '/' && st[i] != '+' && st[i] != '-' {
+			prom += string(st[i])
+		} else if st[i] != ' ' && (st[i] == '*' || st[i] == '/' || st[i] == '+' || st[i] == '-') {
+			lst2 = append(lst2, string(st[i]))
+		} else {
+			lst1 = append(lst1, string(prom))
+			prom = ""
+		}
+	}
+	for len(lst1) > 1 {
+		cnt := 0
+		for i := 0; i < len(lst1); i++ {
+			if lst1[i] == "" {
+				cnt++
+			}
+		}
+
+		if len(lst1)-cnt > 2 {
+			if lst1[1] == "" {
+				y, _ := strconv.ParseFloat(lst1[2], 64)
+				x, _ := strconv.ParseFloat(lst1[0], 64)
+				lst1[2] = virajenia(x, y, lst2[0])
+				lst1 = lst1[2:]
+				lst2 = lst2[1:]
+			} else {
+				y, _ := strconv.ParseFloat(lst1[1], 64)
+				x, _ := strconv.ParseFloat(lst1[0], 64)
+				lst1[1] = virajenia(x, y, lst2[0])
+				lst1 = lst1[1:]
+				lst2 = lst2[1:]
+			}
+		} else {
+			y, _ := strconv.ParseFloat(lst1[len(lst1)-1], 64)
+			x, _ := strconv.ParseFloat(lst1[0], 64)
+			rezk = (virajenia(x, y, lst2[0]))
+			break
+		}
+
+	}
+	return rezk
 }
 
 func main() {
+	// var pocti = ""
 	for _, value := range st {
 		if (value) != ' ' {
 			lst = append(lst, string(value))
 		}
 	}
+	// fmt.Print(lst)
+	var h = 0
 	for i := 0; i < len(lst); i++ {
 		promrez := ""
 		if lst[i] == ")" {
 			for j := i - 1; lst[j] != "("; j-- {
 				promrez += lst[j]
+				h = j
 			}
+			// fmt.Print(revVirajenia(promrez))
+			st := ""
+			s := revVirajenia(promrez)
+			for k := 0; k < len(s); k++ {
+				st += string(s[k])
+			}
+			lst[h-1] = (polski(VoBrtnPolski(st)))
+			lst = append(lst[:h], lst[i+1:]...)
+			// fmt.Print(lst, "\n")
+			i = 0
 			promrez = ""
 		}
-		// заменять скобки в списке на значение из функции которую нужно написать для рассчитывания значений в скобках
-
+		// заменять скобки в списке на значение из функции которую нужно написать для ->
+		// ->рассчитывания значений в скобках
 	}
-	// fmt.Print((PerevodStrVvInt("3")))
+
+	var itog = ""
+	for g := 0; g < len(lst); g++ {
+		itog += lst[g]
+	}
+
+	fmt.Print(polski(VoBrtnPolski(itog)))
+
 }
